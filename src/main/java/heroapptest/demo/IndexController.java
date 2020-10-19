b306257c25dd4594e2350fe1a2942cf806319d04
jacqques <jacqques99@hotmail.com>
2020-10-19 20:03:06 +0200

    ini/final

---
+++ src/main/java/heroapptest/demo/IndexController.java
@@ -0,0 +1,15 @@
+package heroapptest.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String indexController(){
+
+        return "index";
+    }
+
+}
